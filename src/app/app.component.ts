c42943779c9ac96ef66e9271ffbb72e8e82aa94e
justinakdogan <justin.akdogan@radtke-solutions.de>
2022-07-27 18:49:33 +0200

    remove color from task groups

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgxBootstrapIconsModule } from 'ngx-bootstrap-icons';
+import { allIcons } from 'ngx-bootstrap-icons';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'electron-app';
+  title = 'ToDo-App';
+
+
 }
